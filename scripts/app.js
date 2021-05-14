65502044f655c5a0e08b409f8a02d0edcbda89e2
alis <asyed5839@gmail.com>
2021-05-14 21:36:24 +0100

    Added day/night option for images

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,10 @@ const cityForm = document.querySelector(".change-location");
 const card = document.querySelector(".card");
 // Handle to details
 const details = document.querySelector(".details");
+// Image
+const time = document.querySelector("img.time");
+// image icon
+const icon = document.querySelector(".icon");
 // Update city function
 const updateCity = async (city) => {
     console.log(city);
@@ -14,16 +18,28 @@ const updateCity = async (city) => {
     return {cityDetails, weather};
 };
 const updateUI = (data) => {
-    const CityDets = data.cityDetails;
-    const weather = data.weather;
+    // Destructure properties of the object
+    // const CityDets = data.cityDetails;
+    // const weather = data.weather;
+    const { cityDetails, weather } = data;
+
     details.innerHTML = 
-      `<h5>${CityDets.EnglishName}</h5>
+      `<h5>${cityDetails.EnglishName}</h5>
       <div>${weather.WeatherText}</div>
       <div class="w3-padding-large">
         <span>${weather.Temperature.Metric.Value}</span>
         <span>&deg;C</span>
       </div>`;
 
+      // Update the night / Day & icon images
+      let timeSrc = null;
+      if (weather.IsDayTime) {
+          timeSrc = "img/day.svg";
+      }
+      else {
+          timeSrc = "img/night.svg";
+      }
+      time.src = timeSrc;
     // w3-hide
     if (card.classList.contains("w3-hide")) {
         card.classList.remove("w3-hide");
