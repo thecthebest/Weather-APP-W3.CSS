675e84354dd1b19895e7e7c29e74d7319dfc957f
alis <asyed5839@gmail.com>
2021-05-14 21:15:53 +0100

    Added UpdateUI function

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,8 @@
 const cityForm = document.querySelector(".change-location");
 // Handle to card
 const card = document.querySelector(".card");
+// Handle to details
+const details = document.querySelector(".details");
 // Update city function
 const updateCity = async (city) => {
     console.log(city);
@@ -11,11 +13,28 @@ const updateCity = async (city) => {
     // Below is the object short hand notation
     return {cityDetails, weather};
 };
+const updateUI = (data) => {
+    const CityDets = data.cityDetails;
+    const weather = data.weather;
+    details.innerHTML = 
+      `<h5>${CityDets.EnglishName}</h5>
+      <div>${weather.WeatherText}</div>
+      <div class="w3-padding-large">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+      </div>`;
+
+    // w3-hide
+    if (card.classList.contains("w3-hide")) {
+        card.classList.remove("w3-hide");
+    }
+
+};
 cityForm.addEventListener("submit", (e) => {
     e.preventDefault();
     const city = cityForm.city.value.trim();
     cityForm.reset();
     updateCity(city)
-      .then((data) => {console.log(data)})
+      .then((data) => {updateUI(data);})
       .catch();
 });
