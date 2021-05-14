6104de7c975b681e20bccdcb212a3ca38bbde976
alis <asyed5839@gmail.com>
2021-05-14 15:50:40 +0100

    API request complete

---
+++ scripts/forecast.js
@@ -0,0 +1,27 @@
+const key = "3vMt2nSqDWipJWKDE58xBDIlAVq8ou03";
+
+
+const getCity = async (city) => {
+    const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+    const query = `?apikey=${key}&q=${city}`;
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data[0];
+}
+
+const getWeather = async (id) => {
+    const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+    const query = `${id}?apikey=${key}`;
+    const response = await fetch(base + query);
+    const data = await response.json();
+    return data;
+}
+
+// getCity("zurich")
+//   .then((rs) => {
+//       console.log("The city id rece", rs);
+//       return getWeather(rs.Key);
+//   }).then(info => console.log(info))
+//   .catch();
+
+
